cc6eec29311e4b6438bd45888c1a704a1295c91a
Samir Ali <82404683+samir142@users.noreply.github.com>
2023-04-03 17:20:47 +0200

    Contact Finished

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var sliderDots = document.querySelectorAll(".slider .dot");
+
+for (let index = 0; index < sliderDots.length; index++) {
+    const sliderDot = sliderDots[index];
+    sliderDot.onclick = function() {
+        
+    }
+}
