477b28ac4d5c5bc84109b596d2fd1f7414552a55
Samir Ali <82404683+samir142@users.noreply.github.com>
2023-04-10 01:00:31 +0200

    Finished the whole website

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-var sliderDots = document.querySelectorAll(".slider .dot");
+// var sliderDots = document.querySelectorAll(".slider .dot");
 
-for (let index = 0; index < sliderDots.length; index++) {
-    const sliderDot = sliderDots[index];
-    sliderDot.onclick = function() {
+// for (let index = 0; index < sliderDots.length; index++) {
+//     const sliderDot = sliderDots[index];
+//     sliderDot.onclick = function() {
         
-    }
+//     }
+// }
+
+let menu = document.querySelector(".fa-bars"),
+    navLinks = document.querySelector(".navbar-nav");
+
+
+menu.onclick = function() {
+    navLinks.classList.toggle("show");
 }
