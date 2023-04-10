c8499f476e04a68779481c673d482b5684db7ce5
Samir Ali <82404683+samir142@users.noreply.github.com>
2023-04-10 14:50:04 +0200

    Finished the whole website

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-// var sliderDots = document.querySelectorAll(".slider .dot");
 
 // for (let index = 0; index < sliderDots.length; index++) {
-//     const sliderDot = sliderDots[index];
-//     sliderDot.onclick = function() {
+    //     const sliderDot = sliderDots[index];
+    //     sliderDot.onclick = function() {
         
 //     }
 // }
@@ -11,6 +10,30 @@ let menu = document.querySelector(".fa-bars"),
     navLinks = document.querySelector(".navbar-nav");
 
 
-menu.onclick = function() {
-    navLinks.classList.toggle("show");
+    menu.onclick = function() {
+        navLinks.classList.toggle("show");
+}
+///////////////////////////////////////////////////////////////
+
+var sliderDots = document.querySelectorAll(".slider .dot"),
+    containers = document.querySelectorAll(".news .row");
+
+
+
+for (let index = 0; index < containers.length; index++) {
+    const containerElement = containers[index];
+    for (let index = 0; index < sliderDots.length; index++) {
+        const sliderDot = sliderDots[index];
+        sliderDot.addEventListener("click", () => {
+            for (let index = 0; index < sliderDots.length; index++) {
+                const element = sliderDots[index];
+                element.classList.remove("active");
+                containerElement.classList.remove("show");
+            }
+            sliderDot.classList.add("active");
+            if(sliderDot.getAttribute("data-count") === containerElement.getAttribute("data-count")) {
+                containerElement.classList.add("show");
+            }
+        })
+    }
 }
